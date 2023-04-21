b70208308eb3b3d6e03dd9f7863e3a86e15e329b
EyxOfc <108961402+EyxOfc@users.noreply.github.com>
2023-04-20 22:29:01 -0300

    Up

--- src/Comps/Button.jsx
+++ src/Comps/Button.jsx
@@ -6,7 +6,7 @@ function Button({Text, Func}) {
             <button 
                 type="submit"
                 onClick={Func}
-                className='transition .3s flex flex-row flex-nowrap items-center justify-center py-[10px] px-[20px] mx-auto my-[15px] text-[.8rem] font-Mona bg-sky-500 hover:bg-sky-400 gap-[5px] rounded-sm shadow-md hover:shadow-xl'
+                className='transition .3s sm:scale-[1.2] flex flex-row flex-nowrap items-center justify-center py-[10px] px-[20px] mx-auto text-[.8rem] font-Mona bg-sky-500 hover:bg-sky-400 gap-[5px] rounded-sm shadow-md hover:shadow-xl'
             >
                 {Text}
             </button>
--- src/Comps/Title.jsx
+++ src/Comps/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Title({Text}) {
     return ( 
         <>
-            <h1 className='w-full my-[10px] font-Hubot font-[900] text-center text-[1.5rem] sm:text-[1.75rem] md:text-[2rem] lg:text-[2.25rem]'>{Text}</h1>
+            <h1 className='w-full font-Hubot font-[900] text-center text-[1.5rem] sm:text-[1.75rem] md:text-[2rem] lg:text-[2.25rem]'>{Text}</h1>
         </>
     );
 }
